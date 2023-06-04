ff00a0a24abcf1caf748d381a2d73c861ea5cd38
Armin Veres <armin.veres@hotmail.com>
2023-06-04 15:11:52 +0200

    feat(sofle): reduced tap term to 150 ms + bootloader mapping to knob

--- keyboards/sofle/keymaps/arminveres/config.h
+++ keyboards/sofle/keymaps/arminveres/config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+/// Reduce Tapping term from the default 200ms to 150ms as I use it everywhere else for better
+/// reaction
+/// To do individual keys: https://docs.qmk.fm/#/tap_hold?id=tapping-term
+#ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 150
+#endif
+
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
