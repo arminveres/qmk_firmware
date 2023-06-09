0edcc1714d59296f482da6ca403c483f93742f2c
Armin Veres <armin.veres@hotmail.com>
2023-06-09 09:32:42 +0200

    feat(sofle): experimenting with Home Row Mods!
    Remapped home row, increased tapping term. Although will maybe need to increase even more.

--- keyboards/sofle/keymaps/arminveres/config.h
+++ keyboards/sofle/keymaps/arminveres/config.h
@@ -1,14 +1,16 @@
-#pragma once
-
-/// Reduce Tapping term from the default 200ms to 150ms as I use it everywhere else for better
-/// reaction
-/// To do individual keys: https://docs.qmk.fm/#/tap_hold?id=tapping-term
-#ifdef TAPPING_TERM
-#    undef TAPPING_TERM
-#    define TAPPING_TERM 150
-#endif
-
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
+#pragma once
+
+/// Increase Tapping term from the default 200ms to 220ms for better fast typing!
+/// To do individual keys: https://docs.qmk.fm/#/tap_hold?id=tapping-term
+#ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#    define TAPPING_TERM 220
+#endif
+
+/// In combination with fast typing and home row mods, PERMISSIVE_HOLD enables to register keys instead of layer changes
+/// For more information see: https://docs.qmk.fm/#/tap_hold?id=tap-or-hold-decision-modes
+#define PERMISSIVE_HOLD
