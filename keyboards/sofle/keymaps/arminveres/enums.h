74b8631fea29d63b08a01f39cd654b42e335225d
Armin Veres <armin.veres@hotmail.com>
2023-10-05 12:01:44 +0200

    feat(sofle): remove timeout for shift keys on home row

--- keyboards/sofle/keymaps/arminveres/enums.h
+++ keyboards/sofle/keymaps/arminveres/enums.h
@@ -1,13 +1,15 @@
 #pragma once
 #include QMK_KEYBOARD_H // IWYU pragma: keep
 
+// clang-format off
 // Create an X macro to define layers and their names for printing later on OLED
 #define MY_LAYERS                      \
-    X_LAYER(_QWERTY, "Qwrt", "Base")   \
+    X_LAYER(_QWERTY, "Qwrt", "Base\n") \
     X_LAYER(_GAMING, "Game", "Gaming") \
-    X_LAYER(_LOWER, "", "Lower")       \
-    X_LAYER(_UPPER, "", "Upper")       \
-    X_LAYER(_ADJUST, "", "Adjust")
+    X_LAYER(_LOWER,  "----", "Lower")   \
+    X_LAYER(_UPPER,  "----", "Upper")   \
+    X_LAYER(_ADJUST, "----", "Adjst")
+// clang-format on
 
 #define X_LAYER(layer, mode_name, layer_name) layer,
 enum eSofleLayers { MY_LAYERS };
@@ -20,9 +22,4 @@ enum eCustomKeycodes {
     KC_QWERTY = SAFE_RANGE,
     KC_GAMING,
     KC_COLEMAK,
-    // KC_PRVWD,
-    // KC_NXTWD,
-    // KC_LSTRT,
-    // KC_LEND,
-    // KC_DLINE
 };
--- keyboards/sofle/keymaps/arminveres/keymap.c
+++ keyboards/sofle/keymaps/arminveres/keymap.c
@@ -4,36 +4,25 @@
 #include "sentence_case.h"
 #include "home_row.h"
 
-/*
- * Transparent layer
-[_LOWER] = LAYOUT(
-  _______,  _______,  _______,  _______,  _______,  _______,                       _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,                       _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______,                       _______,  _______,  _______,  _______,  _______,  _______,
-  _______,  _______,  _______,  _______,  _______,  _______, _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-                       _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______
-),
- */
-
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌───────────────┬────────┬─────────────┬────────┬────────┬─────┐                             ┌─────┬────────┬────────┬────────┬───────────┬───────────────┐
-//    │      esc      │   1    │      2      │   3    │   4    │  5  │                             │  6  │   7    │   8    │   9    │     0     │     bspc      │
-//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┤                             ├─────┼────────┼────────┼────────┼───────────┼───────────────┤
-//    │      tab      │   q    │      w      │   e    │   r    │  t  │                             │  y  │   u    │   i    │   o    │     p     │       \       │
-//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┤                             ├─────┼────────┼────────┼────────┼───────────┼───────────────┤
-//    │     lctl      │ HOME_A │   HOME_S    │ HOME_D │ HOME_F │  g  │                             │  h  │ HOME_J │ HOME_K │ HOME_L │ HOME_SCLN │   RCTL_T(')   │
-//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┼────────────┐   ┌────────────┼─────┼────────┼────────┼────────┼───────────┼───────────────┤
-//    │ OSM(MOD_LSFT) │   z    │      x      │   c    │   v    │  b  │    mute    │   │            │  n  │   m    │   ,    │   .    │     /     │ OSM(MOD_RSFT) │
-//    └───────────────┴────────┼─────────────┼────────┼────────┼─────┼────────────┤   ├────────────┼─────┼────────┼────────┼────────┼───────────┴───────────────┘
-//                             │ MO(_ADJUST) │  ralt  │  lgui  │ spc │ MO(_LOWER) │   │ MO(_LOWER) │ ent │  lgui  │  ralt  │        │                            
-//                             └─────────────┴────────┴────────┴─────┴────────────┘   └────────────┴─────┴────────┴────────┴────────┘                            
+//    ┌───────────────┬────────┬─────────────┬────────┬────────┬─────┐                             ┌─────┬────────┬────────┬─────────────┬───────────┬───────────────┐
+//    │      esc      │   1    │      2      │   3    │   4    │  5  │                             │  6  │   7    │   8    │      9      │     0     │     bspc      │
+//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┤                             ├─────┼────────┼────────┼─────────────┼───────────┼───────────────┤
+//    │      tab      │   q    │      w      │   e    │   r    │  t  │                             │  y  │   u    │   i    │      o      │     p     │       \       │
+//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┤                             ├─────┼────────┼────────┼─────────────┼───────────┼───────────────┤
+//    │     lctl      │ HOME_A │   HOME_S    │ HOME_D │ HOME_F │  g  │                             │  h  │ HOME_J │ HOME_K │   HOME_L    │ HOME_SCLN │   RCTL_T(')   │
+//    ├───────────────┼────────┼─────────────┼────────┼────────┼─────┼────────────┐   ┌────────────┼─────┼────────┼────────┼─────────────┼───────────┼───────────────┤
+//    │ OSM(MOD_LSFT) │   z    │  RALT_T(x)  │   c    │   v    │  b  │    mute    │   │     no     │  n  │   m    │   ,    │  RALT_T(.)  │     /     │ OSM(MOD_RSFT) │
+//    └───────────────┴────────┼─────────────┼────────┼────────┼─────┼────────────┤   ├────────────┼─────┼────────┼────────┼─────────────┼───────────┴───────────────┘
+//                             │ MO(_ADJUST) │   no   │  lgui  │ spc │ MO(_LOWER) │   │ MO(_LOWER) │ ent │  lgui  │   no   │ MO(_ADJUST) │                            
+//                             └─────────────┴────────┴────────┴─────┴────────────┘   └────────────┴─────┴────────┴────────┴─────────────┘                            
 [_QWERTY] = LAYOUT(
-  KC_ESC        , KC_1   , KC_2        , KC_3    , KC_4    , KC_5   ,                               KC_6   , KC_7    , KC_8    , KC_9    , KC_0      , KC_BSPC        ,
-  KC_TAB        , KC_Q   , KC_W        , KC_E    , KC_R    , KC_T   ,                               KC_Y   , KC_U    , KC_I    , KC_O    , KC_P      , KC_BSLS        ,
-  KC_LCTL       , HOME_A , HOME_S      , HOME_D  , HOME_F  , KC_G   ,                               KC_H   , HOME_J  , HOME_K  , HOME_L  , HOME_SCLN , RCTL_T(KC_QUOT),
-  OSM(MOD_LSFT) , KC_Z   , KC_X        , KC_C    , KC_V    , KC_B   , KC_MUTE    ,     _______    , KC_N   , KC_M    , KC_COMM , KC_DOT  , KC_SLSH   , OSM(MOD_RSFT)  ,
-                           MO(_ADJUST) , KC_RALT , KC_LGUI , KC_SPC , MO(_LOWER) ,     MO(_LOWER) , KC_ENT , KC_LGUI , KC_RALT , _______                              
+  KC_ESC        , KC_1   , KC_2         , KC_3    , KC_4    , KC_5   ,                               KC_6   , KC_7    , KC_8    , KC_9           , KC_0      , KC_BSPC        ,
+  KC_TAB        , KC_Q   , KC_W         , KC_E    , KC_R    , KC_T   ,                               KC_Y   , KC_U    , KC_I    , KC_O           , KC_P      , KC_BSLS        ,
+  KC_LCTL       , HOME_A , HOME_S       , HOME_D  , HOME_F  , KC_G   ,                               KC_H   , HOME_J  , HOME_K  , HOME_L         , HOME_SCLN , RCTL_T(KC_QUOT),
+  OSM(MOD_LSFT) , KC_Z   , RALT_T(KC_X) , KC_C    , KC_V    , KC_B   , KC_MUTE    ,     XXXXXXX    , KC_N   , KC_M    , KC_COMM , RALT_T(KC_DOT) , KC_SLSH   , OSM(MOD_RSFT)  ,
+                           MO(_ADJUST)  , XXXXXXX , KC_LGUI , KC_SPC , MO(_LOWER) ,     MO(_LOWER) , KC_ENT , KC_LGUI , XXXXXXX , MO(_ADJUST)                                 
 ),
 
 //    ┌───────────────┬───┬─────────────┬──────┬──────┬─────┐                             ┌─────┬──────┬──────┬─────┬───┬───────────────┐
@@ -95,12 +84,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 // clang-format on
 
-#if 0
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _LOWER, _GAMING, _ADJUST);
-}
-#endif
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_achordion(keycode, record)) {
         return false;
@@ -121,132 +104,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return false;
         }
-#if 0
-        case KC_PRVWD:
-            if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    register_mods(mod_config(MOD_LALT));
-                    register_code(KC_LEFT);
-                } else {
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_LEFT);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LALT));
-                    unregister_code(KC_LEFT);
-                } else {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_LEFT);
-                }
-            }
-            break;
-        case KC_NXTWD:
-            if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    register_mods(mod_config(MOD_LALT));
-                    register_code(KC_RIGHT);
-                } else {
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_RIGHT);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LALT));
-                    unregister_code(KC_RIGHT);
-                } else {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_RIGHT);
-                }
-            }
-            break;
-        case KC_LSTRT:
-            if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    // CMD-arrow on Mac, but we have CTL and GUI swapped
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_LEFT);
-                } else {
-                    register_code(KC_HOME);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_LEFT);
-                } else {
-                    unregister_code(KC_HOME);
-                }
-            }
-            break;
-        case KC_LEND:
-            if (record->event.pressed) {
-                if (keymap_config.swap_lctl_lgui) {
-                    // CMD-arrow on Mac, but we have CTL and GUI swapped
-                    register_mods(mod_config(MOD_LCTL));
-                    register_code(KC_RIGHT);
-                } else {
-                    register_code(KC_END);
-                }
-            } else {
-                if (keymap_config.swap_lctl_lgui) {
-                    unregister_mods(mod_config(MOD_LCTL));
-                    unregister_code(KC_RIGHT);
-                } else {
-                    unregister_code(KC_END);
-                }
-            }
-            break;
-        case KC_DLINE:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_LCTL));
-                register_code(KC_BSPC);
-            } else {
-                unregister_mods(mod_config(MOD_LCTL));
-                unregister_code(KC_BSPC);
-            }
-            break;
-        case KC_COPY:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_LCTL));
-                register_code(KC_C);
-            } else {
-                unregister_mods(mod_config(MOD_LCTL));
-                unregister_code(KC_C);
-            }
-            return false;
-        case KC_PASTE:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_LCTL));
-                register_code(KC_V);
-            } else {
-                unregister_mods(mod_config(MOD_LCTL));
-                unregister_code(KC_V);
-            }
-            return false;
-        case KC_CUT:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_LCTL));
-                register_code(KC_X);
-            } else {
-                unregister_mods(mod_config(MOD_LCTL));
-                unregister_code(KC_X);
-            }
-            return false;
-            break;
-        case KC_UNDO:
-            if (record->event.pressed) {
-                register_mods(mod_config(MOD_LCTL));
-                register_code(KC_Z);
-            } else {
-                unregister_mods(mod_config(MOD_LCTL));
-                unregister_code(KC_Z);
-            }
-            return false;
-#endif
     }
     return true;
+}
+
+// ================================================================================================
+/// Achordion Configuration
+// ================================================================================================
+// https://getreuer.info/posts/keyboards/achordion/index.html
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+    // Basically ignore only for shift on homerow, so more eagerly applied than the
+    // achordion_eager_mod from Achordion
+    switch (tap_hold_keycode) {
+        case HOME_J:
+        case HOME_F:
+            return 0; // Bypass Achordion for these keys.
+    }
+    return 1000; // Otherwise use a timeout of 800 ms.
 }
 
 void matrix_scan_user(void) {
     achordion_task();
 }
+
+#if 0
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _GAMING, _ADJUST);
+}
+#endif
