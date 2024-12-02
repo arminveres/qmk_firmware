9c1b6e1c8d3ae6e44fc805edd202b708b6160be8
Armin Veres <armin.veres@hotmail.com>
2024-12-02 18:23:04 +0100

    fix: tap_dance to INTROSPECTION_KEYMAP_C

--- keyboards/zsa/moonlander/keymaps/arminveres/rules.mk
+++ keyboards/zsa/moonlander/keymaps/arminveres/rules.mk
@@ -1,8 +1,8 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 TAP_DANCE_ENABLE = yes
+INTROSPECTION_KEYMAP_C = tap_dance.c
 
-SRC += \
-	tap_dance.c
+# SRC += \
 	# WARN(aver): rgb does not have the stuff it need to be compiled anymore
 	# rgb.c \
